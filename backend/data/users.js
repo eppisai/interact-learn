bfcbb2d3aa9edaa8b3b8a20a80550c55196975da
Aman <54789531+eppisai@users.noreply.github.com>
2021-04-17 06:30:34 +0530

    object schema in progress, saxena kuch commit mat krna abhi conflict aa jai ga

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isCreator: true,
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+
+export default users
