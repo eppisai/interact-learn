f1d80e00697e36ae2ea6f50ef9e4ad5f76a71a87
Akashsaxena2308 <lordmartin2308@gmail.com>
2021-04-18 11:56:44 +0530

    user screen and download screen final

---
+++ frontend/src/components/Download.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Iframe from 'react-iframe'
+
+export default function ConvertToPPT() {
+        return <Iframe url="https://one111111.herokuapp.com/"
+        id="videop"
+        className="myClassname"
+        display="initial"
+        position="absolute"/>
+    
+}
